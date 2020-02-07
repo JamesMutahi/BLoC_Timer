60d838e3a0a3ef4f1a4d94a38a5fd8ecc89b4a28
James Mutahi <mutahijames0@gmail.com>
2020-02-07 14:02:55 +0300

    Set Bloc's dependency on ticker and subscribe

--- lib/timer/timer_bloc.dart
+++ lib/timer/timer_bloc.dart
@@ -1,8 +1,17 @@
+import 'dart:async';
+
+import 'package:bloc_timer/ticker.dart';
 import 'package:bloc_timer/timer/index.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class TimerBloc extends Bloc<TimerEvent, TimerState> {
+  final Ticker _ticker;
   final int _duration = 60;
+
+  StreamSubscription<int> _tickerSubscription;
+
+  TimerBloc({@required Ticker ticker}) : assert(ticker != null), _ticker = ticker;
 
   @override
   TimerState get initialState => Ready(_duration);
